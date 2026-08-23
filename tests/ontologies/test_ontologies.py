e8b7ddc89b693674c2af84e84ec4a212741bd4d1
Andrew Zolensky <andrewlzolensky@gmail.com>
2026-08-23 18:26:06 -0400

    changed ontologies test to read from saved data

--- tests/ontologies/test_ontologies.py
+++ tests/ontologies/test_ontologies.py
@@ -14,68 +14,23 @@ def standardized_measurement(measurement, concept, concept_ancestor):
     return std
 
 @pytest.fixture(scope="session")
-def meds_events(
-    spark,
-    concept,
-    concept_ancestor,
-    condition_occurrence,
-    death,
-    drug_exposure,
-    standardized_measurement,
-    observation,
-    person,
-    procedure_occurrence,
-    visit_occurrence
-):
-    # register OMOP data tables
-    tables = [
-       condition_occurrence,
-        death,
-        drug_exposure,
-        observation,
-        person,
-        procedure_occurrence,
-        visit_occurrence,
-        standardized_measurement
-    ]
+def meds_events(spark):
+    REPO_ROOT = Path(__file__).resolve().parents[2]
+    MEDS_DATA_DIR = REPO_ROOT / os.environ["MEDS_DATA_DIR"] / "generated-standard"
+    meds_events_path = MEDS_DATA_DIR / "meds_events.parquet"
+    return spark.read.parquet(str(meds_events_path), schema=schemas.MEDS_EVENT_SCHEMA)
 
-    # record table names
-    table_names = ["condition_occurrence", "death", "drug_exposure", "observation", "person", "procedure_occurrence", "visit_occurrence", "measurement"]
-
-    # extract events
-    event_dfs = []
-    for table, name in zip(tables, table_names):
-        result = extract_events(table, name, measurements_prestandardized=False)
-        event_dfs.append(result)
-
-    # gather events together
-    gathered_events = gather_events(event_dfs)
-
-    # prune events
-    pruned_events = prune_events(gathered_events)
-
-    # post process events
-    post_processed_events = post_process_events(pruned_events)
-
-    # format events
-    formatted_events = format_events(post_processed_events)
-
-    return formatted_events
 
 @pytest.fixture(scope="session")
-def meds_concept_schema(
-    spark,
-    concept,
-    concept_ancestor,
-    meds_events
-):
-
-    # compute concept schema
-    concept_schema = create_concept_schema(meds_events, concept, concept_ancestor)
-
-    return concept_schema
+def meds_concept_schema(spark):
+    REPO_ROOT = Path(__file__).resolve().parents[2]
+    MEDS_DATA_DIR = REPO_ROOT / os.environ["MEDS_DATA_DIR"] / "generated-standard"
+    meds_events_path = MEDS_DATA_DIR / "meds_concept_schema.parquet"
+    return spark.read.parquet(str(meds_events_path), schema=schemas.MEDS_CONCEPT_SCHEMA)
 
 def test_ontologies(spark, meds_events, meds_concept_schema):
+
+    # create ontology and compute
     ontology = Ontology()
     ontology.compute_concept_ontology(meds_events, meds_concept_schema)
     ontology.bin_measurements(meds_events)
